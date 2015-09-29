e3f0739efd93e4ad6e70d04f1f2bfb30efd3486a
Jacob Ebey <jacob.ebey@live.com>
2015-09-29 03:25:59 -0700

    Initial Commit

---
+++ Xaml.Controls/Helpers/IProvideViewCreators.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Xaml.Controls
+{
+    /// <summary>
+    /// This interface provides a means of geting a Func to generate a view from an item.
+    /// The item is also provided to <see cref="Xaml.Controls.IProvideViewCreators.GetViewCreator"/> to select
+    /// the proper view creator for the item.
+    /// </summary>
+    public interface IProvideViewCreators
+    {
+        /// <summary>
+        /// Gets the view creator for the provided item.
+        /// </summary>
+        /// <returns>The view creator.</returns>
+        /// <param name="item">The item to check to get the correct view creator.</param>
+        Func<object, View> GetViewCreator(object item);
+    }
+}
+
+
